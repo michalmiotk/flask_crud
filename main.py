ee08a8e859fb08c418529eaa8f201d3e07dba591
michalmiotk <michalmiotk95@gmail.com>
2022-06-22 12:49:09 +0200

    working with flash

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, flash, url_for
 from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__, template_folder='templates')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
+app.secret_key = 'secret'
 db = SQLAlchemy(app)
 
 class User(db.Model):
@@ -13,6 +15,8 @@ class User(db.Model):
     def __init__(self, username, email):
         self.username = username
         self.email = email
+    def __repr__(self):
+        return str(self.username) + " emails is " + str(self.email)
 
 db.create_all()
 
@@ -24,10 +28,26 @@ def main():
 def create_user():
     return render_template('index.html')
 
+@app.route("/all_users")
+def get_all_users():
+    return Users.Query.all()
+
 @app.route('/internal_create_user', methods=['POST'])
 def internal_create_user():
     user = User(request.form.get('username'), request.form.get('email'))
-    return 'hello'
+    users_with_usernames = User.query.filter_by(username=user.username)
+    users_with_emails = User.query.filter_by(email=user.email)
+    if users_with_usernames:
+        flash("Username already exist" ,'error')
+        return redirect(url_for('create_user'))
+    if users_with_emails:
+        flash("Email already exist" ,'error')
+        return redirect(url_for('create_user'))
+    if True:
+        db.session.add(user)
+        db.session.commit()
+        created_user = User.query.filter_by(username=user.username)
+        return 'user created' + str(created_user)
 
 if __name__ == '__main__':
     app.run(debug=True)
